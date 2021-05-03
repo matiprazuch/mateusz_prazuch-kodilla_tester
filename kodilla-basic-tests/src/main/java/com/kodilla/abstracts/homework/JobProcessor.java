b74da8ab682ad76f1064897470c612d62be81eb6
Mateusz Prazuch <mateuszprazuch1990@gmail.com>
2021-05-03 16:43:01 +0200

    Update

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/JobProcessor.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/JobProcessor.java
@@ -2,6 +2,6 @@ package com.kodilla.abstracts.homework;
 
 public class JobProcessor {
     public void process(Person person) {
-        System.out.println("Responsibilities of " + person.getFirstName() + " are: " + person.getJob().getResponsibilities() + " and his salary is: " + person.getJob().getSalary());
+        System.out.println("Responsibilities of " + person.getFirstName() + " are: " + person.getJob().getResponsibilities() + ". His salary is: " + person.getJob().getSalary());
     }
 }
